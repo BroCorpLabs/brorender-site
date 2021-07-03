02f6e0c79c57c2a428d7f365a5ddc91523085823
Pranith Hengavalli <pranith_h@yahoo.com.sg>
2021-07-03 20:51:49 +0530

    changed testimonial text

--- client/src/components/TestimonialsSection.js
+++ client/src/components/TestimonialsSection.js
@@ -14,22 +14,22 @@ function TestimonialsSection(props) {
       avatar: "https://uploads.divjoy.com/pravatar-150x-5.jpeg",
       name: "Sarah Kline",
       testimonial:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
-      company: "Company",
+        "We use BroRender to save a lot of time while fine-tuning our 3D scenes.",
+      company: "Rikoso Modelworks",
     },
     {
       avatar: "https://uploads.divjoy.com/pravatar-150x-48.jpeg",
       name: "Shawna Murray",
       testimonial:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad amet inventore hic beatae, quas accusantium perferendis sapiente explicabo, corporis totam!",
-      company: "Company",
+        "Thanks to BroRender, my team no longer needs powerful gaming PCs just to render their models!",
+      company: "Balena Architecture",
     },
     {
       avatar: "https://uploads.divjoy.com/pravatar-150x-12.jpeg",
       name: "Blake Elder",
       testimonial:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad amet inventore hic beatae.",
-      company: "Company",
+        "The distributed render feature is much faster while designing cutscenes than our in-house render farm.",
+      company: "Red Gaming",
     },
   ];
 
